0efae7cd266c2797da67160f74c8ed8cc0f96421
Alex Black <wa7@sa34.cx>
2021-09-20 15:01:47 +0300

    try teloxide

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use dotenv::dotenv;
+use teloxide::prelude::*;
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+
+    teloxide::enable_logging!();
+    log::info!("Starting dices_bot...");
+
+    let bot = Bot::from_env().auto_send();
+
+    teloxide::repl(bot, |message| async move {
+        message.answer_dice().await?;
+        respond(())
+    })
+    .await;
 }
